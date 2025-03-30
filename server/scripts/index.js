ed63f803104f18b5a829e90d9f3ee44a1b062e0f
Edward Nedelcu <edward.n9343@gmail.com>
2025-03-30 13:18:29 +0300

    working on layout

---
+++ server/scripts/index.js
@@ -0,0 +1,13 @@
+console.log("Movies working");
+
+const relativeItem = document.querySelectorAll(".relative");
+const itemHover = document.querySelectorAll(".item_hover");
+
+relativeItem.forEach((element) => {
+  element.addEventListener("mouseover", (e) => {
+    element.querySelector("ul").classList.remove("hidden");
+  });
+  element.addEventListener("mouseout", () => {
+    element.querySelector("ul").classList.add("hidden");
+  });
+});
