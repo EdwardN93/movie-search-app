b4968726e78036be6d3f95d8904dae0427611f42
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-04-02 00:12:07 +0300

    fixed links and added my account page

--- server/public/scripts/check-links.js
+++ server/public/scripts/check-links.js
@@ -1,12 +1,16 @@
 const showLogin = document.querySelector("#refLog");
 const showSignUp = document.querySelector("#refSign");
+const subMediaPrimary = document.querySelector(".primary");
+
+const checkForToken = () => {
+  const token = localStorage.getItem("token");
+  return token;
+};
 
 const checkIfLoggedIn = () => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
+  if (checkForToken()) {
     showLogin.textContent = "Log out";
-    showLogin.addEventListener("click", (e) => {
+    showLogin.addEventListener("click", () => {
       localStorage.clear();
       window.location.href = `http://192.168.1.137:3000/index.html`;
     });
@@ -14,4 +18,16 @@ const checkIfLoggedIn = () => {
   }
 };
 
+const showAccountPage = () => {
+  if (checkForToken()) {
+    const liAccountPage = document.createElement("li");
+    const accountPage = document.createElement("a");
+    accountPage.href = `http://192.168.1.137:3000/account.html`;
+    accountPage.textContent = `My Account`;
+    liAccountPage.append(accountPage);
+    subMediaPrimary.append(liAccountPage);
+  }
+};
+
 checkIfLoggedIn();
+showAccountPage();
