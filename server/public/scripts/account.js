f2afb5ff3909ef0ac58a674b0b2f26696faf7e91
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-04-04 22:36:38 +0300

    working on getting favorites from db

--- server/public/scripts/account.js
+++ server/public/scripts/account.js
@@ -26,14 +26,55 @@ async function fetchUserDetails() {
       `http://192.168.1.137:3000/users/${loggedInUser.sub}`,
       options
     );
+    const getMovies = await fetch(`http://192.168.1.137:3000/movies/`, options);
 
     if (!response.ok) throw new Error(`Error: ${response.statusText}`);
 
     const userData = await response.json();
+    const moviesJSON = await getMovies.json();
+    const filteredMovies = moviesJSON.filter((movie) =>
+      userData.favorites.includes(movie.id)
+    );
 
     const userNameDisplay = document.querySelector(".profile-name");
     const favoriteMovies = document.querySelector(".favorites");
     const watchlist = document.querySelector(".watchlist");
+    const itemsWrapper = document.querySelector(".items-wrapper");
+
+    console.log(filteredMovies);
+    filteredMovies.forEach((movie) => {
+      const htmlTemplate = `
+    <div id="card" class="item-card">
+      <div class="card-image">
+        <a href="#">
+          <img src="${movie.img}" alt="img" />
+        </a>
+      </div>
+      <div class="card-details">
+        <div class="details-wrapper">
+          <div class="movie-title">
+            <h2><a href="#">${movie.title}</a></h2>
+            <span>${movie.released}</span>
+          </div>
+          <div class="movie-description">
+            <p>
+              ${movie.overview}
+            </p>
+          </div>
+          <div class="action-bar">
+            <ul class="action-bar-list">
+              <li>Favorite</li>
+              <li>Watchlist</li>
+              <li>Remove</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+`;
+
+      itemsWrapper.insertAdjacentHTML("beforeend", htmlTemplate);
+    });
 
     userNameDisplay.textContent = userData.name || "No Name Found";
 
@@ -74,78 +115,10 @@ const addToFavs = async () => {
     const userData = await response.json();
     console.log(userData);
 
-    userData.favorites.push(101, 202, 303);
+    // userData.favorites.push(101, 202, 303);
   } catch (error) {
     console.error("Failed to fetch user details:", error);
   }
 };
-
-const addFavoritesToList = async () => {
-  const loggedInUser = getLoggedInUser();
-  const token = localStorage.getItem("token");
-
-  if (!loggedInUser) return;
-
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${JSON.parse(token)}`,
-    },
-  };
-
-  try {
-    const response = await fetch(
-      `http://192.168.1.137:3000/users/${loggedInUser.sub}`,
-      options
-    );
-
-    if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-
-    const userData = await response.json();
-    console.log(userData);
-
-    userData.favorites.forEach((element) => {
-      const html = `
-            <div id="card" class="item-card">
-              <div class="card-image">
-                <a href="#">
-                  <img src="imgs/interstellar.jpg" alt="img" />
-                </a>
-              </div>
-              <div class="card-details">
-                <div class="details-wrapper">
-                  <div class="movie-title">
-                    <h2><a href="#">Movie Name</a></h2>
-                    <span>21 oct 2020</span>
-                  </div>
-                  <div class="movie-description">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Architecto placeat itaque quibusdam nisi dolorum obcaecati
-                      dignissimos? Assumenda, saepe vero? Nulla provident, in
-                      voluptas at harum sed maxime tempora accusantium quasi.
-                    </p>
-                  </div>
-                  <div class="action-bar">
-                    <ul class="action-bar-list">
-                      <li>Favorite</li>
-                      <li>Watchlist</li>
-                      <li>Remove</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-`;
-    });
-
-    userData.favorites.push(101, 202, 303);
-  } catch (error) {
-    console.error("Failed to fetch user details:", error);
-  }
-};
-
-// addToFavs();
 
 fetchUserDetails();
