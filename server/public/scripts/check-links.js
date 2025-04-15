b6a34b72167142b9fb5dc082c111c3646451ea21
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-04-16 00:01:13 +0300

    added admin page link based on account type

--- server/public/scripts/check-links.js
+++ server/public/scripts/check-links.js
@@ -4,7 +4,7 @@ const subMediaPrimary = document.querySelector(".primary");
 
 const checkForToken = () => {
   const token = localStorage.getItem("token");
-  return token;
+  return JSON.parse(token);
 };
 
 const checkIfLoggedIn = () => {
@@ -18,7 +18,39 @@ const checkIfLoggedIn = () => {
   }
 };
 
-const showAccountPage = () => {
+const getCurrentUser = () => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  const [, payload] = token.split(".");
+  return JSON.parse(atob(payload));
+};
+
+const checkCurrentAccountType = async () => {
+  const loggedInUser = getCurrentUser();
+  if (!loggedInUser) return;
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${checkForToken()}`,
+    },
+  };
+  try {
+    const res = await fetch(
+      `http://192.168.1.137:3000/users/${loggedInUser.sub}`,
+      options
+    );
+
+    const user = await res.json();
+
+    showAccountPageAndAdminPage(user.accType);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const showAccountPageAndAdminPage = (user = "user") => {
   if (checkForToken()) {
     const liAccountPage = document.createElement("li");
     const accountPage = document.createElement("a");
@@ -26,8 +58,17 @@ const showAccountPage = () => {
     accountPage.textContent = `My Account`;
     liAccountPage.append(accountPage);
     subMediaPrimary.append(liAccountPage);
+
+    if (user === "admin") {
+      const liAdminPage = document.createElement("li");
+      const adminPage = document.createElement("a");
+      adminPage.href = "#"; // `http://192.168.1.137:3000/admin.html`;
+      adminPage.textContent = `Admin Settings`;
+      liAdminPage.append(adminPage);
+      subMediaPrimary.append(liAdminPage);
+    }
   }
 };
 
+checkCurrentAccountType();
 checkIfLoggedIn();
-showAccountPage();
